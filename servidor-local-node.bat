46bac041623082de415b8e5acd0f200a6daaaaae
Lucas <lucasfariassantos45@gmail.com>
2025-12-29 15:20:34 -0300

    Limpeza do projeto - removendo arquivos desnecessários

--- servidor-local-node.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando servidor local para o sistema web...
-echo Acesse: http://localhost:3001
-echo Pressione Ctrl+C para parar o servidor
-cd sistema-web
-npx http-server -p 3001
--- servidor-local.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando servidor local para o sistema web...
-echo Acesse: http://localhost:8000
-echo Pressione Ctrl+C para parar o servidor
-cd sistema-web
-python -m http.server 8000
